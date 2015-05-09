d15d60625c28e15bee122a63c987c92b27e1bab0
adamyarger <yargeradam@gmail.com>
2015-05-09 14:38:36 -0500

    adds create action to recipes controller

--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
-require 'spec_helper'
+# require 'spec_helper'
 
 describe RecipesController, type: :controller do
+
+	include Devise::TestHelpers
 
 	it { should use_before_action(:authenticate_user!) }
 	it { should_not use_before_action(:prevent_ssl) }
 
-	# TODO (Adam Yarger): add POST #create test
-	
+	describe 'POST #create' do
+	    context 'with valid attributes' do
+			before(:each) do
+				@request.env["devise.mapping"] = Devise.mappings[:user]
+				user = FactoryGirl.create :user
+				sign_in user
+			    # @recipe_attributes = FactoryGirl.attributes_for :recipe
+			end
+	    	
+			it 'creates the recipe' do
+				post :create, recipe: attributes_for(:recipe)
+				expect(Recipe.count).to eq(1)
+			end
+
+			it 'redirects to the "show" action for the new recipe' do
+				post :create, recipe: attributes_for(:recipe)
+				expect(response).to redirect_to Recipe.first
+			end
+	    end
+	end
 	
 end
