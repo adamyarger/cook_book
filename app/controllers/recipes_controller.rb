a5f20c8d1f8aa7c58739a711a9bddd35cf87db18
adamyarger <yargeradam@gmail.com>
2015-05-08 19:27:02 -0500

    add new and show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,43 @@
 class RecipesController < ApplicationController
+
+	before_action :authenticate_user!, only: [:create, :update, :destroy]
+
+	def create
+		@recipe = current_user.recipes.build(recipe_params)
+
+		if @recipe.save
+			flash[:success] = "Recipe Created!"
+			redirect_to @recipe
+		else
+			flash[:alert] = "Somthing went wrong"
+			render 'static_pages/root'
+		end
+	end
+
+	def index
+		@recipes = Recipe.all
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	def show
+		@recipe = Recipe.find(params[:id])
+	end
+
+	private
+
+		def recipe_params
+			params.require(:recipe).permit(:title, :description, :ingredients, :time, :directions, :servings)
+		end
+
 end
--- spec/controllers/recipes_controller_spec.rb
+++ spec/controllers/recipes_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe RecipesController, :type => :controller do
+describe RecipesController do
 
+	it { should use_before_action(:authenticate_user!) }
+	it { should_not use_before_action(:prevent_ssl) }
+
+	describe 'POST #create' do
+		it "creates a new recipe" do expect{ post :create, recipe: Factory.attributes_for(:recipe) 
+			}.to change(recipe,:count).by(1) 
+		end 
+	end
 end
