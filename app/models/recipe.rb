5f07beb4138e76bc20d2a298af7af151e02a2914
adamyarger <yargeradam@gmail.com>
2015-05-12 18:51:33 -0500

    adds missing picture default

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,8 +3,8 @@ class Recipe < ActiveRecord::Base
 
 	belongs_to :user
 
-	validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 2.megabytes
-    has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    										 :default_url => "/images/:style/missing.png"
+		validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 2.megabytes
+	  has_attached_file :picture, :styles => { :medium => "250x250#", :thumb => "100x100#" }, 
+	                                                    :default_url => ":style/missing_recipe.png"
   	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ActiveRecord::Base
 
     validates_with AttachmentSizeValidator, :attributes => :avatar, :less_than => 2.megabytes
     has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-    										 :default_url => "/images/:style/missing.png"
+    										 :default_url => "/images/:style/missing_avatar.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
