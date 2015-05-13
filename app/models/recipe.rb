1e3619668025aad5b335ef82236175a692595c11
adamyarger <yargeradam@gmail.com>
2015-05-13 14:24:12 -0500

    adds callbacks to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,10 +1,27 @@
 class Recipe < ActiveRecord::Base
 	validates :title, presence: true
 
+	after_save :load_into_soulmate
+	before_destroy :remove_from_soulmate
+
 	belongs_to :user
 
-		validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 2.megabytes
-	  has_attached_file :picture, :styles => { :medium => "250x250#", :thumb => "100x100#" }, 
-	                                                    :default_url => ":style/missing_recipe.png"
-  	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+	validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 2.megabytes
+  has_attached_file :picture, :styles => { :medium => "250x250#", :thumb => "100x100#" }, 
+                                                    :default_url => ":style/missing_recipe.png"
+	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
+  	private
+
+  		def load_into_soulmate
+  			loader = Soulmate::Loader.new("recipes")
+  			loader.add("term" => name, "id" => self.id, "data" => {
+  					"link" => Rails.application.routes.url_helpers.recipe_path(self)
+  				})
+  		end
+
+  		def remove_from_soulmate
+  			loader = Soulmate::Loader.new("recipes")
+  			loader.remove("id" => self.id)
+  		end
 end
